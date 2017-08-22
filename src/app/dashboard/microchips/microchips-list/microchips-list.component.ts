52993346070ffdc403702253ab0af1826fd99d27
Samuel Murillo <samuelmurillo333@gmail.com>
2017-08-22 11:46:46 -0400

    Add material data table for microchip list

--- src/app/dashboard/microchips/microchips-list/microchips-list.component.ts
+++ src/app/dashboard/microchips/microchips-list/microchips-list.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { ApiService } from "../../../shared/api.service";
-import { Microchip } from "../../../shared/microchip";
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk';
+import { MdPaginator } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
 
-declare const $: any;
+import { ApiService } from '../../../shared/api.service';
+import { Microchip } from '../../../shared/microchip';
 
 @Component({
     selector: 'app-microchips',
-    templateUrl: './microchips-list.component.html',
-    styleUrls: ['./microchips-list.component.sass']
+    templateUrl: './microchips-list.component.html'
 })
-export class MicrochipsListComponent implements OnInit, AfterViewInit {
+export class MicrochipsListComponent implements OnInit {
+    @ViewChild('filter') filter: ElementRef;
+    @ViewChild(MdPaginator) paginator: MdPaginator;
+    displayedColumns = ['name', 'tasks', 'ip']; // TODO Add Owner and date added
+    database: MicrochipsData;
+    dataSource: MicrochipTableDataSource | null;
+    dataLength = 0;
 
     microchips: Microchip[];
-    pages: number[] = [];
     loading: boolean;
     microchip_to_delete: Microchip;
 
@@ -20,62 +33,34 @@ export class MicrochipsListComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.loading = true;
-        let error: boolean = false;
-        let meta;
+        let error = false;
         this.apiService.getMicrochips().subscribe(
             data => {
                 this.microchips = data._items;
-                meta = data._meta;
                 for (let i = 0; i < this.microchips.length; i++) {
                     this.apiService.getTaskByMicrochipID(this.microchips[i]._id).subscribe(
-                        data => { this.microchips[i].tasks = data._meta.total }
+                        taskData => this.microchips[i].tasks = taskData._meta.total
                     );
                 }
+
+                this.database = new MicrochipsData(this.microchips);
+                this.dataSource = new MicrochipTableDataSource(this.database, this.paginator);
+                this.dataLength = this.database.data.length;
+                Observable.fromEvent(this.filter.nativeElement, 'keyup')
+                    .debounceTime(150)
+                    .distinctUntilChanged()
+                    .subscribe(() => {
+                            if (!this.dataSource) { return; }
+                            this.dataSource.filter = this.filter.nativeElement.value;
+                        }
+                    );
             },
             e => {
                 error = true;
                 console.log('Error:', e);
             },
-            () => {
-                let total = meta.total;
-                let count = 0;
-                while (total > 0) {
-                    count += 1;
-                    this.pages.push(count)
-                    total -= meta.max_results;
-                }
-                // Item actions init
-                setTimeout(function () {
-                    $(function () {
-                        var $itemActions = $(".item-actions-dropdown");
-
-                        $(document).on('click', function (e) {
-                            if (!$(e.target).closest('.item-actions-dropdown').length) {
-                                $itemActions.removeClass('active');
-                            }
-                        });
-
-                        $('.item-actions-toggle-btn').on('click', function (e) {
-                            e.preventDefault();
-
-                            var $thisActionList = $(this).closest('.item-actions-dropdown');
-
-                            $itemActions.not($thisActionList).removeClass('active');
-
-                            $thisActionList.toggleClass('active');
-                        });
-                    });
-                }, 500);
-
-                this.loading = false;
-            }
+            () => this.loading = false
         );
-    }
-
-    ngAfterViewInit() {
-        document.getElementById("app").classList.add("header-fixed");
-        document.getElementById("app").classList.add("sidebar-fixed");
-        document.getElementById("app").classList.remove("sidebar-open");
     }
 
     onClickDeleteMicrochip(index: number) {
@@ -85,9 +70,51 @@ export class MicrochipsListComponent implements OnInit, AfterViewInit {
     deleteMicrochip(microchip: Microchip) {
         this.apiService.deleteMicrochip(microchip).subscribe(
             data => this.microchips.splice(this.microchips.indexOf(microchip), 1),
-            error => console.log(error),
-            () => {}
+            error => console.log(error)
         );
     }
+}
 
+
+class MicrochipsData {
+    /** Stream that emits whenever the data has been modified. */
+    dataChange: BehaviorSubject<Microchip[]> = new BehaviorSubject<Microchip[]>([]);
+    get data(): Microchip[] { return this.dataChange.value; }
+    constructor(private _microchips: Microchip[]) {
+        for (const task of this._microchips) {
+            const copiedData = this.data.slice();
+            copiedData.push(task);
+            this.dataChange.next(copiedData);
+        }
+    }
+}
+
+class MicrochipTableDataSource extends DataSource<any> {
+    get filter(): string { return this._filterChange.value; }
+    set filter(filter: string) { this._filterChange.next(filter); }
+    private _filterChange = new BehaviorSubject('');
+
+    constructor(private _microchipsData: MicrochipsData, private _paginator: MdPaginator) {
+        super();
+    }
+
+    /** Connect function called by the table to retrieve one stream containing the data to render. */
+    connect(): Observable<Microchip[]> {
+        const displayDataChanges = [
+            this._microchipsData.dataChange,
+            this._paginator.page,
+            this._filterChange
+        ];
+
+        return Observable.merge(...displayDataChanges).map(() => {
+            // Grab the page's slice of data.
+            const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+            return this._microchipsData.data.slice().splice(startIndex, this._paginator.pageSize).filter((item: Microchip) => {
+                const searchStr = (item.name + item.ip).toLowerCase();
+                return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
+            });
+        });
+    }
+
+    disconnect() {}
 }
